c2bb6f072b21766e6673343b9002a71d69fcd764
masalim2 <masalim2@illinois.edu>
2017-04-19 10:04:31 -0500

    phonon argparser

--- tools/phonon.py
+++ tools/phonon.py
@@ -3,11 +3,16 @@ from pyfrag.Globals import lattice
 import numpy as np
 import itertools
 import sys
+import argparse
+import os
 
 cau = (2.99792458*2.418884326505)*1.0e-9  # c in meters per t(a.u.)
 amu2au = (1.66053892/9.10938291)*1.0e4  # 1 amu in a.u. (via kgs)
 cmtom = 1.0/100.0
 SCALING = cmtom*(1.0/(2*np.pi*cau*amu2au**0.5))
+
+def pretty(x):
+    return "%9.3f" % x
 
 def shift_com_to_origin():
     com = sum([at.pos for at in geom.geometry])
@@ -15,7 +20,7 @@ def shift_com_to_origin():
     for at in geom.geometry:
         at.pos -= com
 
-def make_brillouin_zone3D():
+def make_brillouin_zone3D(kmesh_density=None):
     '''Brute-force generate uniform grid inside 1st Brillouin zone.
 
     Brute force but clean approach. Truncates a uniform rectangular grid;
@@ -23,6 +28,8 @@ def make_brillouin_zone3D():
     sample adequately near the zone boundaries.
     '''
 
+    if kmesh_density is None:
+        kmesh_density = 21
     TOL = 1.0e-5
     lat_vecs = lattice.lat_vecs
     a1, a2, a3 = lat_vecs.T
@@ -42,7 +49,7 @@ def make_brillouin_zone3D():
     assert cell_vecs.shape == (len(cells), 3)
     boxmin = np.array([np.min(cell_vecs[:,i]) for i in range(3)])
     boxmax = np.array([np.max(cell_vecs[:,i]) for i in range(3)])
-    xp, yp, zp = (np.linspace(boxmin[i], boxmax[i], 21) for i in range(3))
+    xp, yp, zp = (np.linspace(boxmin[i], boxmax[i], kmesh_density) for i in range(3))
     grid = np.vstack(np.meshgrid(xp, yp, zp)).reshape(3,-1).T
 
     # For each reciprocal lattice vector:
@@ -140,14 +147,14 @@ def Dmat(hess_mw, kvec, smooth_mat=None):
         rvec = a*Ra + b*Rb + c*Rc
         arg = 1.0j*np.dot(kvec, rvec)
         d += (hess_mw[cell]+smooth_mat) * np.exp(arg)
-    assert np.allclose(d, np.conj(d.T))
+    #assert np.allclose(d, np.conj(d.T))
     return d
 
-def phonons(hess_mw, kmesh, smooth_mat=None):
+def phonon(hess_mw, kmesh, smooth_mat=None):
     freqs = []
     vecs = []
+    print "# Phonons at", len(kmesh), "k-points"
     for kvec in kmesh:
-        print "phonons at k", kvec
         d = Dmat(hess_mw, kvec, smooth_mat)
         eigvals, eigvecs = np.linalg.eigh(d)
         freqs.append(np.sign(eigvals)*SCALING*
@@ -157,18 +164,73 @@ def phonons(hess_mw, kmesh, smooth_mat=None):
         vecs.append(eigvecs)
     return freqs, vecs
 
-def pretty(x):
-    return "%9.3f" % x
+def phonon_freqs(hess_mw, kmesh, smooth_mat=None):
+    freqs = []
+    print "# Phonons at", len(kmesh), "k-points"
+    for kvec in kmesh:
+        d = Dmat(hess_mw, kvec, smooth_mat)
+        eigvals = np.linalg.eigvalsh(d)
+        freqs.append(np.sign(eigvals)*SCALING*
+                np.sqrt(np.sign(eigvals)*eigvals))
+    return freqs
 
+def parseargs():
+    parser = argparse.ArgumentParser(description = 
+    'Calculate phonons for given crystal structure and interaction force constants')
+    parser.add_argument('structure', help='.xyz structure file')
+    parser.add_argument('hessian', nargs='?', default=None, help='.hess data file')
+    parser.add_argument('--bz_only', action='store_true',
+                        help='only generate and print 1st BZ kmesh')
+    parser.add_argument('--bz_file', type=str,
+                        help='read existing kmesh from file')
+    parser.add_argument('--kmesh_density', type=int,
+        help='points per dimension in rectangular k-grid before truncation')
+    parser.add_argument('--smooth', action='store_true',
+                        help='force acoustic branches to zero at gamma point')
+    parser.add_argument('--binary_file', type=str,
+                        help='save freqs as binary numpy array')
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
-    geom.load_geometry(sys.argv[1])
-    shift_com_to_origin()
-    bz = make_brillouin_zone3D()
-    hess = read_force_consts_txt(sys.argv[2])
-    hess_mw = mass_weight(hess)
-    freqs, vecs = phonons(hess_mw, bz)
+    
+    args = parseargs()
+    #geom.load_geometry(open(args.structure).read().replace('H','D'))
+    geom.load_geometry(args.structure)
 
-    for kvec, freqs in zip(bz, freqs):
+    if args.bz_file:
+        bz = np.loadtxt(args.bz_file)
+        assert bz.shape[1] == 3 and len(bz.shape) == 2
+        print "# Loaded %d kpoints from file" % len(bz)
+    else:
+        bz = make_brillouin_zone3D(args.kmesh_density)
+        print "# Generated %d kpoints in 1st BZ" % len(bz)
+
+    if args.bz_only:
+        for kvec in bz:
+            print kvec[0], kvec[1], kvec[2]
+        sys.exit(0)
+
+    if args.hessian is None:
+        raise RuntimeError("Need input hessian file")
+    if args.binary_file:
+        assert not os.path.exists(args.binary_file), "file already exists"
+
+    shift_com_to_origin()
+    hess = read_force_consts_txt(args.hessian)
+    print "# Read interaction force consts in %d cells" % len(hess.keys())
+    hess_mw = mass_weight(hess)
+
+    if args.smooth:
+        smooth_mat = smoothing_matrix(hess_mw)
+    else:
+        smooth_mat = None
+
+    #freqs, vecs = phonons(hess_mw, bz, smooth_mat)
+    dispersion = phonon_freqs(hess_mw, bz, smooth_mat)
+
+    for kvec, freqs in zip(bz, dispersion):
         print " ".join([pretty(k) for k in kvec]),
         print " ".join([pretty(f) for f in freqs])
+    if args.binary_file:
+        np.savez(args.binary_file, kmesh=bz, freqs=np.array(dispersion))
